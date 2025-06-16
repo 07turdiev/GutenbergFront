47e8479b19f4575c4b1b4227f89deb2175209ef0
Elbek <elbekturdiyev20020707@gmail.com>
2025-06-16 20:26:22 +0500

    Home gape joylashuvi
    Home page mobil qismiga moslandi. Yana bitta hatolik paginatsiya qismi ko'rinmay qolmoqda

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,8 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* Viewport meta tag for mobile responsiveness */}
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
           {/* Sitka font import */}
           <link
             href="https://fonts.googleapis.com/css2?family=Sitka+Text:ital,wght@0,400;0,600;0,700;1,400;1,600;1,700&display=swap"
