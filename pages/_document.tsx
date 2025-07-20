6c334fe02b6c3f37b29c35630def3a09e9393b69
Elbek <elbekturdiyev20020707@gmail.com>
2025-07-20 12:46:38 +0500

    Mobil rangini singash uchun o'zgartirildi

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,14 +7,17 @@ class MyDocument extends Document {
         <Head>
           {/* Mobile Browser Address Bar Theming */}
           <meta name="theme-color" content="#cd1b17" />
+          <meta name="msapplication-TileColor" content="#cd1b17" />
           
           {/* iOS Safari Specific */}
           <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="GutenberNU" />
           
           {/* Additional mobile optimizations */}
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5, user-scalable=yes" />
           <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="format-detection" content="telephone=no" />
           
           {/* Windows Phone */}
           <meta name="msapplication-navbutton-color" content="#cd1b17" />
@@ -22,8 +25,22 @@ class MyDocument extends Document {
           {/* PWA Manifest */}
           <link rel="manifest" href="/manifest.json" />
           
-          {/* Apple Touch Icon */}
+          {/* Apple Touch Icons - Multiple sizes for better iOS support */}
           <link rel="apple-touch-icon" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="57x57" href="/favicon.png" />
+          
+          {/* Favicon */}
+          <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon.png" />
           
           {/* Sitka font import */}
           <link
